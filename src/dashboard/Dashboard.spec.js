f3687c7dc349f4b8d7bb2f1f371de609f85ab4f8
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-06 15:12:03 -0700

    test dashboard is rendering display and controls

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,11 +1,35 @@
 // Test away
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, cleanup } from 'react-testing-library';
+import renderer from 'react-test-renderer';
 
 import Dashboard from './Dashboard';
 
+afterEach(cleanup);
+
 describe('<Dashboard />', () => {
   it('should render w/o crashing', () => {
-    const tmp = render(<Dashboard />);
+    render(<Dashboard />);
   });
+
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders display', () => {
+    const { getByText } = render(<Dashboard />);
+    // initial state is locked: false, closed: false
+    // displays 'unlocked' and 'open'
+    expect(getByText(/unlocked/i)).toBeInTheDocument;
+    expect(getByText(/open/i)).toBeInTheDocument;
+  });
+
+  it('renders controls', () => {
+    const { getByText } = render(<Dashboard />);
+    // initial state is locked: false, closed: false
+    // displays 'lock gate' and 'close gate' buttons
+    expect(getByText(/lock gate/i)).toBeInTheDocument;
+    expect(getByText(/close gate/i)).toBeInTheDocument;
+  })
 });
