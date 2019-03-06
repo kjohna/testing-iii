092a4b76fb1fa18328aa23509a63f6a60dd548ac
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-06 15:56:05 -0700

    add tests for proper class used. comments added

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,9 +1,11 @@
 // Test away!
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, cleanup } from 'react-testing-library';
 import renderer from 'react-test-renderer';
 
 import Display from './Display';
+
+afterEach(cleanup);
 
 describe ('<Display />', () => {
   it('renders w/o crashing', () => {
@@ -17,25 +19,51 @@ describe ('<Display />', () => {
 
   // Display's props default to:
   // { closed: false, locked: false }
+
+  // test gate open: text and class
+  // default props work for this
   it('displays "open" when gate is open', () => {
-    // default props work for this
     const { getByText } = render(<Display />);
     expect(getByText(/open/i)).toBeInTheDocument;
   });
 
+  it('uses "green-led" class when gate is open', () => {
+    const { getByText } = render(<Display />);
+    // console.log(getByText(/open/i).classList);
+    expect(getByText(/open/i).classList.contains('green-led')).toBe(true);
+  });
+
+  // test gate closed: text and class
   it('displays "closed" when gate is closed', () => {
     const { getByText } = render(<Display closed={true} />);
     expect(getByText(/closed/i)).toBeInTheDocument;
   });
 
+  it('uses "red-led" class when gate is closed', () => {
+    const { getByText } = render(<Display closed={true} />);
+    expect(getByText(/closed/i).classList.contains('red-led')).toBe(true);
+  })
+
+  // test gate unlocked: text and class
+  //default props work here
   it('displays "unlocked" when gate is unlocked', () => {
-    //default props work here
     const { getByText } = render(<Display />);
     expect(getByText(/unlocked/i)).toBeInTheDocument;
   });
 
+  it('uses "green-led" class when gate is unlocked', () => {
+    const { getByText } = render(<Display />);
+    expect(getByText(/unlocked/i).classList.contains('green-led')).toBe(true);
+  });
+
+  // test gate locked: text and class
   it('displays "locked" when gate is locked', () => {
     const { getByText } = render(<Display locked={true} />);
     expect(getByText(/locked/i)).toBeInTheDocument;
+  });
+
+  it('uses "red-led" class when gate is locked', () => {
+    const { getByText } = render(<Display locked={true} />);
+    expect(getByText(/locked/i).classList.contains('red-led')).toBe(true);
   });
 });
